a89d35f62675a56add824a6d4c7c58d321e42e0b
saurabh <webdeveloperess3@expostandservice.com>
2025-06-19 17:38:09 +0530

    update route

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,23 @@ use App\Http\Controllers\ShowController;
 
 Route::get('/', function() {
     return view('home');
-});
+})->name('home');
 
 Route::get('/about-us', function() {
     return view('about-us');
-});
+})->name('about-us');
 
 Route::get('/contact-us', function() {
     return view('contact-us');
-});
+})->name('contact-us');
 
 Route::get('/privacy-policy', function() {
     return view('privacy-policy');
-});
+})->name('privacy-policy');
 
 Route::get('/login', function() {
     return view('login');
-});
+})->name('login');
 
 Route::get('/blogs', [BlogController::class, 'index'])->name('blogs');
 
