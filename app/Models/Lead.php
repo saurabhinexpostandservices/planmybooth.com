f286b69e3c3591e52a536e97ed7f9bd7dfa05ed9
saurabh <webdeveloperess3@expostandservice.com>
2025-07-14 11:36:46 +0530

    update the stucture of leads table

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -15,17 +15,19 @@ class Lead extends Model
         'show_id',
         'city_id',
         'stand_size',
-        'stand_size_measurement_unit',
-        'services',
+        'company_name',
         'price_range',
+        'services',
         'design_attachments',
         'require_elements',
         'employee_onsite_avilable',
+        'message',
+        'page_url',
+        'ip',
         'author_id'
     ];
 
     protected $casts = [
-        'price_range' => 'array',
         'design_attachments' => 'array',
         'require_elements' => 'array',
     ];
